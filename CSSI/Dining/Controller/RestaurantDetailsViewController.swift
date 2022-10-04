970308c16da7b6c1ca13415c789f38963cd2a42f
ZeeshanAM <zeeshanam@ecssi.com>
2022-10-04 19:04:02 +0530

    Added RestaurantDetailsVC

---
+++ CSSI/Dining/Controller/RestaurantDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantDetailsViewController.swift
+//  CSSI
+//
+//  Created by Admin on 10/4/22.
+//  Copyright © 2022 yujdesigns. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
